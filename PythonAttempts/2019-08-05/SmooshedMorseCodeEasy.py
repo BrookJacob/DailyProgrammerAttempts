4b72f55921133e25e31ca855c2a596f5db34723e
Jacob Brook <brookjacob@gmail.com>
2019-11-09 20:04:26 -0800

    Added 2019-08-05 comment description

--- PythonAttempts/2019-08-05/SmooshedMorseCodeEasy.py
+++ PythonAttempts/2019-08-05/SmooshedMorseCodeEasy.py
@@ -1,4 +1,17 @@
-from itertools import product
+#For the purpose of this challenge, Morse code
+# represents every letter as a sequence of 1-4
+# characters, each of which is either . (dot)
+# or -(dash). The code for the letter a is .-,
+# for b is -..., etc. The codes for each letter
+# a through z are:
+# .- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..
+# Normally, you would indicate where one letter
+# ends and the next begins, for instance with a
+# space between the letters' codes, but for this
+# challenge, just smoosh all the coded letters
+# together into a single string consisting of only
+# dashes and dots.
+
 morseAlphabet = str(".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..").split(" ")
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 keywords = ["sos", "daily", "programmer", "bits", "three"]
